2b72cf0c63f3e1064bc7c258bc8c6b3a25d87e3b
Metta Rolando <mnrolando3@gmail.com>
2022-06-22 14:03:59 -0400

    Got display to replace correct letter at correct index and to end game if word is guessed

--- mystery_word.py
+++ mystery_word.py
@@ -29,23 +29,23 @@ def submit_guess():
     user_guess = input('Guess a letter: ')
     return user_guess
 
-# def make_display(file):
-#     display = ['_'*len(file)]
-#     return display
-
 def track_state(file):
     correct_letters=[]
     incorrect_letters=[]
+    display = [(letter.replace(letter, "_")) for letter in file]
+    print(display)
     while len(incorrect_letters) < 8:
-        print ('_'*(len(file)-len(correct_letters)) + str(correct_letters))
         letter = submit_guess()
         if letter.isalpha() and len(letter) == 1:
             lowercase_guess = letter.lower()
-            # print(f"You guessed '{lowercase_guess}'")
             if lowercase_guess in file:
                 correct_letters.append(lowercase_guess)
                 print(f'Correct! {lowercase_guess} is in the word.')
-                # print('_'*(len(file)-len(correct_letters)) + str(correct_letters))
+                display = [(letter.replace(letter, "_")) if letter not in correct_letters else letter for letter in file]
+                print(display)
+                if display == correct_letters:
+                    print('You won!')
+                    break
             else: 
                 print(f'{lowercase_guess} is not in the word.')
                 incorrect_letters.append(lowercase_guess)
@@ -62,14 +62,9 @@ def play_game():
     computer_word = pick_word(new_version)
     print(computer_word)
     # prints the word randomly selected by the pick_word function
-
     letters_to_guess = make_letter_list(computer_word)
     # print(letters_to_guess)
     # # prints letters of the randomly selected word as a list
-
-    # display_letters = make_display(letters_to_guess)
-    # print(display_letters)
-
     track_state(letters_to_guess)
 
 if __name__ == "__main__":
