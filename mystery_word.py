c34f0862bf88a34b7d2efd72a93b164afe23dc67
Metta Rolando <mnrolando3@gmail.com>
2022-06-22 22:39:22 -0400

    Fixed end game to register win and to tell user word.

--- mystery_word.py
+++ mystery_word.py
@@ -53,9 +53,11 @@ def track_state(file):
             if lowercase_guess in file:
                 if lowercase_guess not in correct_letters:
                     correct_letters.append(lowercase_guess)
-                    print(f'\nCorrect! {lowercase_guess} is in the word.\n')
+                    print(f'\nYay! {lowercase_guess} is in the word.\n')
                     # if guess is in file and not in correct letters already
                     # add to correct letters list and replace in display
+                    display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
+                    print(display)
                     if display == file:
                         print('\nYou won!')
                         break
@@ -63,23 +65,29 @@ def track_state(file):
                         # break loop
                 else:
                     print('\nYou already guessed that. Try again.\n')
+                    display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
+                    print(display)
             else:
                 if lowercase_guess not in incorrect_letters:
                     print(f'\n{lowercase_guess} is not in the word. \n')
                     incorrect_letters.append(lowercase_guess)
+                    display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
+                    print(display)
                     # if guess is not in file
                     # add to incorrect letters list
                 else:
                     print('\nYou already guessed that. Try again.\n')
+                    display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
+                    print(display)
         else:
             print('\nThat guess wasn\'t valid. Please try again.\n')
             # if submitted guess is not alphabetic or one character
             # tell user
-        display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
-        print(display)
+            display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
+            print(display)
         print(f'Letters already guessed:{incorrect_letters + correct_letters}\n')
         # print concatenated list of letters already guessed
-    print('Game over!')
+    print(f'Game over! The word was {file}.')
     # game ends when incorrect guesses is 8
 
 
@@ -98,6 +106,7 @@ def play_game():
     # establishes var to call make_letter_list with previous var as arg
     # print(letters_to_guess)
     # # prints letters of the randomly selected word as a list
+    print('\nWelcome to Mystery Word! On the screen is a list of spaces. Guess letters to complete the word. You have 8 incorrect guesses. Repeated or invalid guesses will not count against you.\n')
     track_state(letters_to_guess)
     # calls track_state with previous variable as argument
 
