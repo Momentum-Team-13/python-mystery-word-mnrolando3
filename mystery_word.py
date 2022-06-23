782f7fd576072861506064004975ead77186da8c
Metta Rolando <mnrolando3@gmail.com>
2022-06-22 21:11:08 -0400

    Updated to correct words file and removed print of mystery word. Cleaned up linting and quotations.

--- mystery_word.py
+++ mystery_word.py
@@ -3,7 +3,7 @@ import random
 
 
 def open_file():
-    file = 'test-word.txt'
+    file = 'words.txt'
     # print(f'Your file is: {file}')
     with open(file) as open_file:
         read_file = open_file.readlines()
@@ -43,33 +43,38 @@ def submit_guess():
 def track_state(file):
     correct_letters = []
     incorrect_letters = []
-    display = [(letter.replace(letter, "_")) for letter in file]
+    display = [(letter.replace(letter, '_')) for letter in file]
     print(display)
     while len(incorrect_letters) < 8:
         letter = submit_guess()
         if letter.isalpha() and len(letter) == 1:
             lowercase_guess = letter.lower()
-            # if submitted guess is alphabetic and one character, convert to lowercase
+            # if submitted guess is alphabetic and one character
+            # converts to lowercase
             if lowercase_guess in file:
                 if lowercase_guess not in correct_letters:
                     correct_letters.append(lowercase_guess)
                     print(f'Correct! {lowercase_guess} is in the word.')
-                    display = [(letter.replace(letter, "_")) if letter not in correct_letters else letter for letter in file]
+                    display = [(letter.replace(letter, '_')) if letter not in correct_letters else letter for letter in file]
                     print(display)
-                    # if guess is in file and not in correct letters already, add to correct letters list and replace underscore in display
+                    # if guess is in file and not in correct letters already
+                    # add to correct letters list and replace in display
                     if display == correct_letters:
                         print('You won!')
                         break
-                        # if display list matches correct letters list, break loop
+                        # if display list matches correct letters list
+                        # break loop
                 else:
                     print('You already guessed that. Try again.')
             else:
                 print(f'{lowercase_guess} is not in the word.')
                 incorrect_letters.append(lowercase_guess)
-                # if guess is not in file, add to incorrect letters list
+                # if guess is not in file
+                # add to incorrect letters list
         else:
             print('That guess wasn\'t valid. Please try again.')
-            # if submitted guess is not alphabetic or one character, tell user
+            # if submitted guess is not alphabetic or one character
+            # tell user
         print(f'Letters already guessed:{incorrect_letters + correct_letters}')
         # print concatenated list of letters already guessed
     print('Game over!')
@@ -80,19 +85,19 @@ def play_game():
     file_opened = open_file()
     # establishes variable to call open_file function with no argument
     new_version = make_word_list(file_opened)
-    # establishes variable to call make_word_list function with previous var as arg
+    # establishes var to call make_word_list with previous var as arg
     # print('new', new_version)
     # # prints the list with plain strings
     computer_word = pick_word(new_version)
-    # establishes variable to call pick_word function with previous var as arg
+    # establishes var to call pick_word with previous var as arg
     # print(computer_word)
     # # prints the word randomly selected by the pick_word function
     letters_to_guess = make_letter_list(computer_word)
-    # establishes variable to call make_letter_list function with previous var as arg
-    print(letters_to_guess)
-    # prints letters of the randomly selected word as a list
+    # establishes var to call make_letter_list with previous var as arg
+    # print(letters_to_guess)
+    # # prints letters of the randomly selected word as a list
     track_state(letters_to_guess)
-    # calls track_state function with previous variable as argument
+    # calls track_state with previous variable as argument
 
 
 if __name__ == "__main__":
